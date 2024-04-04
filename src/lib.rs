7a8cb01353f90451f4c00f8d5fafe209f4ae59b4
k <k@k.com>
2024-04-04 15:25:25 +0800

    pymodule

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use json_patch::{patch as apply_patch, Patch};
 #[pyclass]
 struct JsonPatchManager {
     original_json: Value,
-    counter: i64
+    counter: i64,
 }
 
 #[pymethods]
@@ -13,7 +13,7 @@ impl JsonPatchManager {
     #[new]
     fn new(initial_json: String) -> PyResult<Self> {
         let original_json: Value = serde_json::from_str(&initial_json).unwrap();
-        Ok(JsonPatchManager { original_json , counter: 0})
+        Ok(JsonPatchManager { original_json, counter: 0 })
     }
 
     fn set_original(&mut self, new_json: String) -> PyResult<()> {
@@ -33,36 +33,8 @@ impl JsonPatchManager {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use serde_json::json;
-    #[test]
-    fn test_new() {
-        let manager = JsonPatchManager::new("{\"name\": \"John\"}".to_string()).unwrap();
-        assert_eq!(manager.original_json, json!({"name": "John"}));
-    }
-
-    #[test]
-    fn test_set_original() {
-        let mut manager = JsonPatchManager::new("{}".to_string()).unwrap();
-        manager.set_original("{\"name\": \"Jane\"}".to_string()).unwrap();
-        assert_eq!(manager.original_json, json!({"name": "Jane"}));
-    }
-
-    #[test]
-    fn test_apply_patch() {
-        let mut manager = JsonPatchManager::new("{\"name\": \"John\"}".to_string()).unwrap();
-        let patch = "[{\"op\": \"replace\", \"path\": \"/name\", \"value\": \"Jane\"}]".to_string();
-        manager.apply_patch(patch).unwrap();
-        assert_eq!(manager.original_json, json!({"name": "Jane"}));
-        assert_eq!(manager.counter, 1);
-    }
-
-    #[test]
-    fn test_get_original() {
-        let manager = JsonPatchManager::new("{\"name\": \"John\"}".to_string()).unwrap();
-        let original_json_str = manager.get_original().unwrap();
-        assert_eq!(original_json_str, "{\"name\":\"John\"}");
-    }
+#[pymodule]
+fn python_rust_json_patch(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<JsonPatchManager>()?;
+    Ok(())
 }
